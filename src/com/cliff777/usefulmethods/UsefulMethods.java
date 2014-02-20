ebc46d199f6b8e12286b6725b777189d46a7f43d
cliff777 <bugman12345@gmail.com>
2014-02-20 15:03:37 -0500

    Added date method

---
+++ src/com/cliff777/usefulmethods/UsefulMethods.java
@@ -0,0 +1,35 @@
+package com.cliff777.usefulmethods;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class UsefulMethods 
+{
+
+	public static void main(String[] args)
+	{
+		new UsefulMethods();
+	}
+
+	public UsefulMethods()
+	{
+
+	}
+
+	//compares a date in string form to the current date
+	//and gives the difference in seconds
+	public int fromThenToNow(String date) throws ParseException
+	{
+		DateFormat format = new SimpleDateFormat("MM-dd-yyyy h:mm");
+
+		Date now = new Date();
+		Date then = format.parse(date);
+
+		long difference = then.getTime() - now.getTime();
+
+		return (int) (difference/1000);
+	}
+
+}
